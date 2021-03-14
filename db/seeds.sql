7a9c7211815eec555f23a5fd8f7d71ef0bb0e782
salfoband90@gmail.com <salfoband90@gmail.com>
2021-03-14 18:03:49 -0400

    created connection.js

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple-Stacked Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cali-Chicken Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Pepper Jack Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', true);
