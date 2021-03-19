a1fd4af0caeaed7bc13b2906b8cbd6796ae62d51
salfoband90@gmail.com <salfoband90@gmail.com>
2021-03-18 22:36:37 -0400

    linked connection to the server

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 var express = require("express");
+
+const connection = require("./config/connection");
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+
+
+app.listen(PORT, function(){
+    console.log("server listening on: http://localhost:" + PORT);
+})
