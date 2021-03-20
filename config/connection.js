1cf60d0ea0ed3c7ed900b45d3704d3eaab88a54c
salfoband90@gmail.com <salfoband90@gmail.com>
2021-03-20 16:51:46 -0400

    added connection db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,24 @@
 var mysql = require("mysql");
+var connection;
+if(!process.env.NODE_ENV || process.env.NODE_ENV=="development"){
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password1",
+        database: "burgers_db"
+      });
+}else{
+    connection = mysql.createConnection({
+        host: "xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port: 3306,
+        user: "ppdoplcm9k496yc4",
+        password: "jymanujc081gjw00",
+        database: "uqjr761tzn868n4r"
+      });
+}
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password1",
-    database: "burgers_db"
-  });
+ 
 
     // Make connection
   connection.connect(function(err) {
